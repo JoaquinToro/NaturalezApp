49224132e3f584a25a1e4c2a506392d69be920c5
lalalaam <agusincardenasalvarez@gmail.com>
2024-10-13 18:37:57 -0300

    Agregando las paginas de quienes somos y parque natural gomez carreño en ionic con su css respectivo

---
+++ IonicNaturalezApp/src/pages/Parques/Parque_Natural_Gomez_Carreño.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonButtons, 
+  IonMenuButton, 
+  IonCard, 
+  IonCardContent, 
+  IonCardHeader, 
+  IonCardTitle, 
+  IonGrid, 
+  IonRow, 
+  IonCol, 
+  IonImg,
+  IonFooter,
+  IonMenu,
+  IonList,
+  IonItem,
+  IonLabel
+} from '@ionic/react';
+import './Parque_Natural_Gomez_Carreño.css';
+import '../theme/bootstrap.css';
+
+//Importar componentes
+import FooterN from '../components/FooterN';
+import HeaderN from '../components/HeaderN';
+
+const Parque_Natural_Gomez_Carreño:React FC = () => {
+    return (
+        <IonPage id="main-content">
+        <HeaderN/>
+        <IonContent id='main' fullscreen className="ion-padding">
+          <h2 className="NombreParque">Parque Natural Gomez Carreño</h2>
+
+          <div className="card">
+            <div className="card-image-container">
+              <img className="card-image" src="public/assets/images/Parque_Natural_Gómez_Carreño.jpg" alt="Parque Natural Gómez Carreño" />
+            </div>
+            <div className="card-content">
+              <p>El Parque Natural de Gómez Carreño es un refugio de belleza natural y biodiversidad, ubicado en una región privilegiada que destaca por su rica flora y fauna. Este parque, conocido por sus paisajes variados y su entorno prístino, ofrece a los visitantes un escape del bullicio urbano y una inmersión en la naturaleza.</p>
+            </div>
+          </div>
+
+          <hr />
+
+          <h2 className="fauna-heading">Fauna</h2>
+          <div className="fauna-grid">
+            <img src="public/assets/images/aguilucho.jpg" alt="Una imagen de un Aguilucho" />
+            <img src="public/assets/images/picalor.jpg" alt="Una imagen mostrando un Picaflor" />
+            <img src="public/assets/images/piden.jpg" alt="Imagen mostrando un Piden" />
+          </div>
+
+          <hr />
+
+          <h2 className="flora-heading">Flora</h2>
+          <div className="flora-grid">
+            <img src="public/assets/images/Chagual Azul.jpg" alt="Una imagen de un Chagual Azul" />
+            <img src="public/assets/images/Flor de la Culebra.jpeg" alt="Una imagen mostrando una Flor de la Culebra" />
+            <img src="public/assets/images/Oreja de Zorro.jpg" alt="Imagen mostrando una flor llamada Oreja de Zorro" />
+          </div>
+
+          <hr />
+        </IonContent>
+        <FooterN/>
+        </IonPage>
+
+    )
+}
