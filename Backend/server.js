49cdd057ff1dae375e800e8c24e9528548ca18a9
Joaquin Toro <joaquintoro53@gmail.com>
2024-10-16 20:57:25 -0300

    Fix llamada a backend en Home

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,16 @@
 const express=require("express");
 const connection=require("./db");
 const bodyParser=require("body-parser");
+const cors=require("cors");
 const app=express();
 const port=3001;
-
+app.use(cors());
 app.use(bodyParser.json());
 
 //metodos
 app.get('/parque-destacado',(req,res)=>{
     console.log("Solicitud para obtener parque destacado...")
-    const query="select * from parque where id_parque = 3";
+    const query="select * from parque";
     connection.query(query,(error, resultado)=>{
         console.log("Parque destacado obtenido!")
         res.json(resultado);
