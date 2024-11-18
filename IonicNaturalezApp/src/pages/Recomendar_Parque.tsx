586f04d0a1691792212d1ec9758a00e66016ac7e
JoaquinToro <joaquintoro53@gmail.com>
2024-11-18 01:20:53 -0300

    Changes to Recomendar_Parque Todavia no esta implementada

--- IonicNaturalezApp/src/pages/Recomendar_Parque.tsx
+++ IonicNaturalezApp/src/pages/Recomendar_Parque.tsx
@@ -1,12 +1,41 @@
-import { IonContent, IonPage } from "@ionic/react";
-import React from "react";
+import { IonCol, IonContent, IonPage, IonRow } from "@ionic/react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import './Recomendar_Parque.css';
 import '../theme/bootstrap.css';
 import FooterN from "../components/FooterN";
 import HeaderN from "../components/HeaderN";
+import { CardInterface } from "../utils/CardInterface";
+import { ResultInterface } from "../utils/ResultInterface";
+import Resultado from "../components/Resultado";
+
 
 const Recomendar_Parque: React.FC = () =>{
+
+    const [busqueda,setBusqueda] = useState('');
+    const [parqueRecomendado,setParqueRecomendado] = useState('')
+    const [filtros,setFiltros] = useState([])
+    const [filtroActual,setFiltroActual] = useState('')
+    const [resultados,setResultados]=useState([])
+
+    const realizarBusqueda = async (cosaABuscar:string) =>{
+        try {
+            const response = await axios.get('http://localhost:3001/buscar-cosa',{
+                params: {
+                    name: cosaABuscar,
+                    table: filtroActual
+                }
+            });
+            
+            setResultados(response.data);
+            
+            console.log(resultados);
+        } catch (error) {
+            console.log("Error");
+        }
+        return;
+    }
 
     return(
         <IonPage>
@@ -16,37 +45,38 @@ const Recomendar_Parque: React.FC = () =>{
                     <h2>Elija la categoría que desea Buscar</h2>
                 <div className="row align-items-center">
                     <div className="col-6 mb-2 tabla">
-                        <button title="Seleccionar flora" type="button" className="btn btn-primary">Flora</button>
+                        <button title="Seleccionar flora" type="button" className="btn btn-primary" onClick={()=>{setFiltroActual('flora')}}>Flora</button>
                     </div>
                     <div className="col-6 mb-2 tabla">
-                        <button title="Seleccionar fauna" type="button" className="btn btn-primary">Fauna</button>
+                        <button title="Seleccionar fauna" type="button" className="btn btn-primary" onClick={()=>{setFiltroActual('fauna')}}>Fauna</button>
                     </div>
                 </div>
                 <div className="row align-items-center">
                     <div title="Seleccionar ubicación" className="col-6 mb-2 tabla">
-                        <button className="btn btn-primary">Ubicacion</button>
+                        <button className="btn btn-primary" onClick={()=>{setFiltroActual('region')}}>Ubicacion</button>
                     </div>
                     <div title="Seleccionar tipo de área" className="col-6 mb-2 tabla">
-                        <button className="btn btn-primary">Tipo</button>
+                        <button className="btn btn-primary" onClick={()=>{setFiltroActual('tipo')}}>Tipo</button>
                     </div>
                 </div>
                 </div>
                 <div className="container-fluid busqueda">
                     <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Ingrese su búsqueda..." aria-label="Search"/>
+                         <button className="btn btn-outline-success" type="button" onClick={()=>realizarBusqueda(busqueda)}>Buscar</button>
                     </form>
                 </div>
                 <div className="container px-4 py-4 bg-white caja-resultados">
-                    <div className="resultado">
-                        <img src="assets/placeholder/placeholder-image.jpg" alt="Imagen de placeholder"/>
-                        <div className="info">
-                            <h3 id="nombre-resultado">Lorem Ipsum</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        </div>
-                    </div>
-                    <div className="agregar">
-                        <button title="Agregar decisión" type="button" className="btn-agregar">Agregar</button>    
-                    </div>  
+                    {
+                        resultados.length > 0 ? resultados.map((r : ResultInterface, index:number)=>{
+                            <IonRow key={r.id || index}>
+                                <IonCol>
+                                    <Resultado r={r} />
+                                </IonCol>
+                            </IonRow>
+                        })
+                        :<></>
+                    }
                 </div>
                 <FooterN/>
             </IonContent>
