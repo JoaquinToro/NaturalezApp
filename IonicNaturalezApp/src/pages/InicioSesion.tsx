8427e77e5fb60432ccb9349e43ef4ba11eaf290f
Diego Salinas <125302353+Salinas03@users.noreply.github.com>
2024-11-19 23:18:13 -0300

    Changed - login, mensaje de datos incorrectos en el inicio de sesion

--- IonicNaturalezApp/src/pages/InicioSesion.tsx
+++ IonicNaturalezApp/src/pages/InicioSesion.tsx
@@ -4,6 +4,7 @@ import './InicioSesion.css';
 
 // Validaciones
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { useState, useEffect } from 'react';
 
 // Define el tipo de datos del formulario
 interface FormValues {
@@ -11,38 +12,50 @@ interface FormValues {
     password: string;
 }
 
-const ingresar: SubmitHandler<FormValues> = async (data) => {
-    console.log("Datos enviados: ",data);
 
-    try{
-        const response = await fetch("http://localhost:3001/inicioSesion", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-
-        if (response.ok){
-            const result = await response.json();
-            console.log("Inicio de sesion exitosa", result);
-
-            // Guardar el token y el nombre de usuario
-            localStorage.setItem('token', result.token);
-            localStorage.setItem('username', result.username);  // Guardar nombre de usuario
-            window.location.href = '/home';  // Redirigir al home o a la ruta protegida
-        }
-        else{
-            console.error("Error al iniciar sesión:", response.statusText);
-        }
-    }catch (error) {
-        console.error("Error de red:", error);
-    }
-}
 
 const InicioSesion: React.FC = () => {
 
     const { handleSubmit, register, formState: { errors } } = useForm<FormValues>();
+    const [ errorMessage, setErrorMessage ] = useState<string | null>(null);
+
+    const ingresar: SubmitHandler<FormValues> = async (data) => {
+        console.log("Datos enviados: ",data);
+    
+        try{
+            const response = await fetch("http://localhost:3001/inicioSesion", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+    
+            if (response.ok){
+                const result = await response.json();
+                console.log("Inicio de sesion exitosa", result);
+    
+                // Guardar el token y el nombre de usuario
+                localStorage.setItem('token', result.token);
+                localStorage.setItem('username', result.username);  // Guardar nombre de usuario
+                window.location.href = '/home';  // Redirigir al home o a la ruta protegida
+            }
+            else{
+                console.error("Error al iniciar sesión:", response.statusText);
+                setErrorMessage("Error al ingresar usuario o contraseña.");
+            }
+        }catch (error) {
+            console.error("Error de red:", error);
+            setErrorMessage("Error al conectar con el servidor.");
+        }
+    }
+
+    
+
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        setErrorMessage(null); // Limpiar mensajes de error anteriores
+        ingresar(data);
+    };
 
     return (
         <IonPage>
@@ -73,6 +86,11 @@ const InicioSesion: React.FC = () => {
                                     <p style={{ marginLeft: '37%', color: '#A5BE7D' }}>Accede a tu cuenta</p>
                                 </div>
 
+                                {errorMessage && (
+                                    <div className="alert alert-danger text-center">
+                                        {errorMessage}
+                                    </div>
+                                )}
 
                                 <button className="btn btn-lg btn-outline-secondary btn-outline-custom btn-lg w-100 mb-3 pt-3" onClick={() => window.location.href = '/  '}>
                                     <i className='bx bxl-google text-danger me-1 fs-6'></i>Ingresa con Google
@@ -95,7 +113,7 @@ const InicioSesion: React.FC = () => {
                                     </div>
                                 </div>
 
-                                <form action="#" onSubmit={handleSubmit(ingresar)}>
+                                <form action="#" onSubmit={handleSubmit(onSubmit)}>
                                     <div className="input-group mb-3">
                                         <span className="input-group-text">
                                             <i className="bx bx-user"></i>
